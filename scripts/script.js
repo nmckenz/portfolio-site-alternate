7aa49565dc7b955abd05d1a2e61177347400c7b8
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2019-12-15 21:17:40 -0500

    Most structure and styling completed. Still requires touch ups and responsive.

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('.slickContainer').slick({
+        slidesToShow: 1,
+        accessibility: true,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        arrows: false,
+        dots: true,
+        infinite: true,
+        pauseOnFocus: true,
+        pauseOnHover: true,
+        swipe: true,
+        // rtl: true
+  });
+});
