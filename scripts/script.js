43077450c237b43fb02e90b1ffbab4d72ef4857f
Nicholas McKenzie <nicholas.a.mckenzie@gmail.com>
2020-01-04 21:18:19 -0500

    Responsive design and some CSS changes.

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+$(function () {
+    // CAROUSEL CONFIGURATION
     $('.slickContainer').slick({
         slidesToShow: 1,
         accessibility: true,
@@ -11,4 +12,16 @@ $(document).ready(function () {
         pauseOnHover: true,
         swipe: true,
   });
+
+  // REGEX
+  // validateEmail = (email) => {
+  //   const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+  //   return regex.test(email)
+  // }
+
+  // $("form").on("submit", () => {
+  //   const userEmail = $("#email").val();
+
+  // })
+
 });
